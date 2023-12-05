4d6ce1f09ec520243aec5971ee40ec56049a2e23
Debayan Sarkar <debayansarkar368@gmail.com>
2023-12-05 10:26:10 +0530

    Initializedv3

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-const ejs=require('ejs');
+const ejs = require('ejs');
+const favicon = require('serve-favicon');
 const path = require('path');
-const elayouts=require('express-ejs-layouts');
+const elayouts = require('express-ejs-layouts');
 const PORT = process.env.PORT || 3300;
 
 //Assets
@@ -10,11 +11,14 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.render('home');
 })
+const faviconPath = path.join(__dirname, 'public', 'favicon.ico');
 
+
+app.use(favicon(faviconPath));
 //set template engine
 app.use(elayouts);
-app.set('views', path.join(__dirname,'/resources/views'));
-app.set('view engine','ejs')
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs')
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
