2a453919cb428c4d7a86aebd7865579eaba8343b
Debayan Sarkar <debayansarkar368@gmail.com>
2023-12-07 14:56:38 +0530

    v2

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,10 @@
   \*****************************/
 /***/ (() => {
 
-
+if (window.location.href.split('/')[3] == "") {
+  document.getElementById('home').classList.add("selected");
+}
+document.getElementById(window.location.href.split('/')[3]).classList.add("selected");
 
 /***/ }),
 
--- server.js
+++ server.js
@@ -5,20 +5,21 @@ const favicon = require('serve-favicon');
 const path = require('path');
 const elayouts = require('express-ejs-layouts');
 const PORT = process.env.PORT || 3300;
+const faviconPath = path.join(__dirname, 'public', 'favicon.ico');
 
 //Assets
 app.use(express.static('public'))
-app.get('/', (req, res) => {
-    res.render('home');
-})
-const faviconPath = path.join(__dirname, 'public', 'favicon.ico');
 
 
 app.use(favicon(faviconPath));
 //set template engine
 app.use(elayouts);
 app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+require('./routes/web')(app);
+// './' -> means current folder
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
